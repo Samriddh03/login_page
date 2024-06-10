74f52a5deefa08e2f93337d4e04942195efe3efa
Samriddh03 <143608960+Samriddh03@users.noreply.github.com>
2024-06-10 21:12:23 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    $host = "localhost";
+    $dbusername ="root";
+    $dbpassword = "";
+    $dbname = "authentication";
+
+
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+    
+
+    if($conn->connect_error) {
+        die("Connection failed: ". $conn->connect_error);
+    }
+    
+
+    $querry = "SELECT *FROM  login WHERE username='$username' AND password='$password'";
+
+    $result = $conn->query($query);
+
+    if($result->num_rows == 1){
+
+        header("Location: success.html");
+        exit();
+
+    }
+
+
+    else{
+         header("Location: error.html");
+        exit();
+    }
+    
+    
+    $conn->close();
+} 
+?>
